bec77b514fc8441dc7272b9df2e080db0873475e
Simon Harrer <simon.harrer@gmail.com>
2012-03-05 10:46:19 +0100

    File had wrong content

--- lib/gmail_extractor/email_formatter/email_csv_formatter.rb
+++ lib/gmail_extractor/email_formatter/email_csv_formatter.rb
@@ -1,35 +1,25 @@
-class EmailXmlFormatter
+require "csv"
 
-  attr_accessor :printer
+module GmailExtractor
 
-  def initialize(printer)
-    @printer = printer
+  class EmailCsvFormatter
+
+    attr_accessor :printer
+
+    def initialize(printer)
+      @printer = printer
+    end
+
+    def header;  end
+
+    def footer;  end
+
+    def email(email)
+      @printer << [email.date, email.from, email.content].to_csv
+    end
+
+    def label(label,limit); end
+
   end
-
-  def header
-    result = "<emails>"
-
-    @printer << result
-  end
-
-  def footer
-    result = "</emails>"
-
-    @printer << result
-    @printer.close
-  end
-
-  def email(email)
-    result = ""
-    result << "<email>"
-    result << "<from>#{email.from}</from>"
-    result << "<date>#{email.date}</date>"
-    result << "<content>#{email.content}</content>"
-    result << "</email>"
-
-    @printer << result
-  end
-
-  def label(label,limit); end
 
 end
